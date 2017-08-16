2097e9e143fc3229bf299ed4b72fb7b6e30c5e15
John <clientreviews215@gmail.com>
2017-08-15 22:42:49 -0400

    Update anonymousbrowsing.py

--- anonymousbrowsing.py
+++ anonymousbrowsing.py
@@ -3,7 +3,10 @@
 #This uses the service Proxychains and obviously Tor.
 #I made this script, but obviously didn't make Proxychains or Tor.
 #To use this type "cd Desktop, then type python anonymousbrowsing.py in your terminal"
-
+#AFTER RUNNING THIS SCRIPT ONCE, OPEN TERMINAL, TYPE IN 'service tor start' THEN TYPE IN 'proxychains firefox' THAT IS ALL YOU NEED TO DO AFTER USING IT ONCE. DO NOT RUN THE SCRIPT AGAIN
+#If you run the script again, you will need to redo everything, so make sure after running the script once, delete it. 
+#When you want to browse anonymously again, all you need to do is type 'service tor start' in terminal, then type in 'proxychains firefox' 
+#And you will be browsing anonymously!
 import os, time
 text = "DNS: When nano pops up, type in nameserver 78.46.223.24 on one line, press enter, and on the other line type in nameserver 162.242.211.137 ctrl + o to save press enter, ctrl + x to quit. Proxychains: Uncomment dynamic_chain by removing the #. Comment out strict_chain by adding a #. Go down till you see socks4 [tab] 127.0.0.1 [space] 9050 under that put in socks5 [tab] 127.0.0.1 [space] 9050. Then do ctrl + o to save, press enter, and ctrl + x to exit."
 saveFile = open('instructions.txt', 'w')
